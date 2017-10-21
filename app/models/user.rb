8bf1ea6e6280f71d584058fe2fc1fbd36adc36a5
Tony <ANCLIST@GMAIL.COM>
2017-10-21 12:19:46 -0400

    Create form for the new appointment view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
   has_many :appointments
   # has_many :dentist, through: :appointments
+
+  validates :name, :email, :phone, :password,  presence: true
+  validates :email, uniqueness: true
 end
