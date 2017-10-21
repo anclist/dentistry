8d58c266b520c6dcd88d10979d7106dfbc83e08c
Tony <ANCLIST@GMAIL.COM>
2017-10-21 12:34:58 -0400

    Set helper methods in the application controller and defined the create method in the appointment controller

--- app/controllers/concerns/appointments_controller.rb
+++ app/controllers/concerns/appointments_controller.rb
@@ -8,4 +8,23 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.new
   end
 
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.user_id = current_user.id
+
+    if @appointment.save
+      flash[:notice] = "Your appointment has been saved."
+      redirect_to appointments_url
+    else
+      render :new
+    end
+  end
+
+
+private
+
+def appointment_params
+  params.require(:appointment).permit(:dentist_id, :appointment_at)
+end
+
 end
