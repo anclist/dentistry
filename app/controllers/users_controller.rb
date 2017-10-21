e02de017876fa85f43c40383b96003d21d32ac40
Tony <ANCLIST@GMAIL.COM>
2017-10-21 11:42:39 -0400

    Users Create Method, appoinmets resouces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Congrtas"
+      session[:user_id] = @user.id
+      redirect_to appointments_url
+    else
+      flash[:alert] = "Please correct mistakes"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :phone)
   end
 end
