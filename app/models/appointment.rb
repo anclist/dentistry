b6737398d99f5f4d791a08e509c71cebbf296662
Tony <ANCLIST@GMAIL.COM>
2017-10-21 17:50:14 -0400

    Sending SMS

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,15 @@ class Appointment < ApplicationRecord
 
   belongs_to :user
   belongs_to :dentist
-  
+
+  def send_confirmation_sms
+    client = Twilio::REST::Client.new(ENV['TWILIO_ID'], ENV['TWILIO_TOKEN'])
+
+    client.api.account.messages.create(
+      from: ENV['PHONE_NUMBER'],
+      to: "+1#{self.user.phone}",
+      body: "You have an appt on #{self.appointment_at} with #{self.dentist.name}"
+    )
+  end
+
 end
